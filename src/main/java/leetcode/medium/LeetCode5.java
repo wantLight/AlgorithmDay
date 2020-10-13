8885c6b350075532c56799846180bc43a1bb3243
xyzzg <1364932323@qq.com>
2020-10-13 20:43:10 +0800

    10.13 准备先刷二叉树。。

--- src/main/java/leetcode/medium/LeetCode5.java
+++ src/main/java/leetcode/medium/LeetCode5.java
@@ -27,14 +27,14 @@ public class LeetCode5 {
         while (high < str.length - 1 && str[high + 1] == str[low]) {
             high++;
         }
-//         定位中间部分的最后一个字符
+//         目前已定位中间部分的最后一个字符 中间的字符串就是low 到 high
         int ans = high;
 //         从中间向左右扩散
         while (low > 0 && high < str.length - 1 && str[low - 1] == str[high + 1]) {
             low--;
             high++;
         }
-//         记录最大长度
+//         记录最大长度（答案~）
         if (high - low > range[1] - range[0]) {
             range[0] = low;
             range[1] = high;
